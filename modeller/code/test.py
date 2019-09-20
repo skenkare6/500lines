f77b8af93e4981d92b8d6d6cb8bf7560b74771bc
arkd <aaronmdewitt@gmail.com>
2019-09-20 11:23:50 -0500

    feat: add mouse click test

--- modeller/code/test.py
+++ modeller/code/test.py
@@ -1,10 +1,10 @@
 import unittest
 
 import numpy
-from OpenGL.GLUT import GLUT_LEFT_BUTTON
-from mock import MagicMock
+from OpenGL.GLUT import GLUT_LEFT_BUTTON, GLUT_DOWN
+from mock import MagicMock, patch
 from viewer import Viewer
-from node import Cube, Sphere, SnowFigure
+from node import Cube, Sphere, SnowFigure, Node
 
 
 # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python/33024979
@@ -62,17 +62,26 @@ class MyTestCase(unittest.TestCase):
         self.v()
         self.assertTrue(self.success)
 
-    # def mouse_interaction_test(self, obj):
-    #     # we need to reverse and get 2d x,y from 3d translation matrix
-    #     start, direction = self.v.get_ray(obj, 10, 10)
-    #     # place the node at the cursor in camera-space
-    #     translation = (start + direction * obj.scene.PLACE_DEPTH)
-    #     # convert the translation to world-space
-    #     pre_tran = numpy.array([translation[0], translation[1], translation[2], 1])
-    #     translation = obj.inverseModelView.dot(pre_tran)
-    #     obj.interaction.handle_mouse_button(self.key, x, y)
-    #
-    #
+    def mouse_interaction_test(self, obj):
+        # 270, 217 is the default x,y of the sphere
+        obj.interaction.handle_keystroke('s', 10, 10)
+        obj.interaction.handle_mouse_button(self.key, GLUT_DOWN, 10, 10)
+        if obj.scene.node_list[3].selected:
+            self.success = True
+
+    def test_pick_sphere(self):
+        self.key = GLUT_LEFT_BUTTON
+        self.node_type = Sphere
+        self.v.interaction_test = self.mouse_interaction_test
+        self.v()
+        self.assertTrue(self.success)
+
+    # def pan_interaction_test(self, obj):
+    #     # 270, 217 is the default x,y of the sphere
+    #     obj.interaction.handle_keystroke('s', 10, 10)
+    #     obj.interaction.handle_mouse_button(self.key, GLUT_DOWN, 10, 10)
+    #     if obj.scene.node_list[3].selected:
+    #         self.success = True
     #
     # def test_pick_sphere(self):
     #     self.key = GLUT_LEFT_BUTTON
@@ -80,6 +89,5 @@ class MyTestCase(unittest.TestCase):
     #     self.v.interaction_test = self.mouse_interaction_test
     #     self.v()
     #     self.assertTrue(self.success)
-
 if __name__ == '__main__':
     unittest.main()
