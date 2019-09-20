81995999bfedf7da14b2fee490b606bafdb4d743
arkd <aaronmdewitt@gmail.com>
2019-09-19 22:33:56 -0500

    fix: check to correct object type in create node tests

--- modeller/code/test.py
+++ modeller/code/test.py
@@ -3,6 +3,7 @@ import unittest
 import numpy
 from mock import MagicMock
 from viewer import Viewer
+from node import Cube, Sphere
 
 
 # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python/33024979
@@ -16,6 +17,7 @@ class MyTestCase(unittest.TestCase):
         self.v.main_loop = MagicMock(name='main_loop')
         self.success = False
         self.key = ''
+        self.node_type = None
 
     def node_interaction_test(self, obj):
         obj.interaction.handle_keystroke(self.key, 10, 10)
@@ -29,28 +31,34 @@ class MyTestCase(unittest.TestCase):
         for node in obj.scene.node_list:
             if isclose(translation[0], node.translation_matrix[0][3]) and \
                 isclose(-translation[1], node.translation_matrix[1][3]) and \
-                isclose(translation[2], node.translation_matrix[2][3]):
+                isclose(translation[2], node.translation_matrix[2][3]) and \
+                isinstance(node, self.node_type):
                 self.success = True
+                break
 
     def test_create_object_sphere(self):
         # with 'S'
         self.key = 's'
+        self.node_type = Sphere
         self.v.interaction_test = self.node_interaction_test
         self.v()
         self.assertTrue(self.success)
         # should check its a sphere and not just that it exists
 
     def test_create_object_cube(self):
-        # with 'D'
-        self.key = 'd'
+        # with 'C'
+        self.key = 'c'
+        self.node_type = Cube
         self.v.interaction_test = self.node_interaction_test
         self.v()
         self.assertTrue(self.success)
         # should check its a cube and not just that it exists
-    #
-    # def test_pan(self):
-    #     # middle click drag
-    #     self.assertEqual(True, False)
+
+    # def test_pick(self):
+    #     self.key = 'd'
+    #     self.v.interaction_test = self.node_interaction_test
+    #     self.v()
+    #     self.assertTrue(self.success)
     #
     # def test_rotate(self):
     #     # right click drag
